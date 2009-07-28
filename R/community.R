6b9425d89db80bedc48888dd80d27c5c064475db
regetz (Jim Regetz) <regetz@nceas.ucsb.edu>
2009-07-28 21:23:11 +0000

    added Simpson's index function from Marc; includes phylo-weighted version (Hardy and Senterre, 2007)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecopd@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/community.R
+++ R/community.R
@@ -14,3 +14,26 @@ richness <- function(phylo4com) {
   sapply(phylo4com, function(x) nrow(tdata(x)))
 }
 
+# simpson's index with and without phylogenetic distances using
+# commmunity matrix 'x', species names as colnames and community names
+# as rownames. phy=FALSE returns traditional simpson's index
+simp.phy <- function(x, tr, phy=TRUE) {
+
+  x <- as.matrix(x)
+  x <- x[, order(colnames(x))]
+  total <- apply(x, 1, sum)
+  x <- sweep(x, 1, total, "/")
+    
+  if (phy==TRUE){
+    phy.mat <- cophenetic(tr)
+    phy.mat <- phy.mat[order(rownames(phy.mat)), order(colnames(phy.mat))]
+    out <- apply(x, 1, function(x) sum((x %o% x)*phy.mat))
+  } else {
+    bin.mat <- matrix(1, dim(x)[2], dim(x)[2])
+    diag(bin.mat) <- 0
+    out <- apply(x, 1, function(x) sum((x %o% x)*bin.mat))
+  }   
+        
+  return(out) 
+}
+

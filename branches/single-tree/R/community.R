4581bdb1e8baa805455db5d80988ca30772524a0
regetz (Jim Regetz) <regetz@nceas.ucsb.edu>
2009-11-14 00:59:20 +0000

    rewrote original simp.phy function as 'simpson' S4 methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecopd@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/single-tree/R/community.R
+++ branches/single-tree/R/community.R
@@ -26,26 +26,3 @@ siteBySpecies <- function(phylo4com, presence.only=FALSE,
 richness <- function(phylo4com, na.rm=FALSE) {
   sapply(phylo4com, function(x) sum(abundance(x)>0, na.rm=na.rm))
 }
-
-# simpson's index with and without phylogenetic distances using
-# commmunity matrix 'x', species names as colnames and community names
-# as rownames. phy=FALSE returns traditional simpson's index
-simp.phy <- function(x, tr, phy=TRUE) {
-
-  x <- as.matrix(x)
-  x <- x[, order(colnames(x)), drop=FALSE]
-  x <- prop.table(x, 1)
-    
-  if (phy==TRUE){
-    phy.mat <- cophenetic(tr)
-    phy.mat <- phy.mat[order(rownames(phy.mat)), order(colnames(phy.mat))]
-    out <- apply(x, 1, function(x) sum((x %o% x)*phy.mat))
-  } else {
-    bin.mat <- matrix(1, dim(x)[2], dim(x)[2])
-    diag(bin.mat) <- 0
-    out <- apply(x, 1, function(x) sum((x %o% x)*bin.mat))
-  }   
-        
-  return(out) 
-}
-
